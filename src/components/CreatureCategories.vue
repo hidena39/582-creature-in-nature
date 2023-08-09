68323e3a8e9d680d9b843711a054370ed445e6b0
VANIERCOLLEGE\2295439 <2295439@edu.vaniercollege.qc.ca>
2023-08-09 08:56:06 -0400

    possibly finished writing test

--- src/components/CreatureCategories.vue
+++ src/components/CreatureCategories.vue
@@ -4,11 +4,23 @@
 // 1.make categorie Array
 // 2.make all cards false
 // 3.toggle true and false when clicked
-// 4.if cards[true] then show cards in CreatureList
-
+// 4.emits
+// 5.if cards[true] then show cards in CreatureList
+@click="" 
 
 export default {
     name: "CreatureCategories",
+    data() { 
+        return {
+            showing: false,
+        };
+    },
+    methods: {
+        sendShow() { 
+            this.showing = true;
+
+        },
+    },
 
 };
 </script>
