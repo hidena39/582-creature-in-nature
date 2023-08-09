34975ad54b31f5454e069616f97a51d60969e9d2
VANIERCOLLEGE\2295439 <2295439@edu.vaniercollege.qc.ca>
2023-08-09 08:57:02 -0400

    possibly finished writing test

--- tests/unit/CreatureCategories.spec.js
+++ tests/unit/CreatureCategories.spec.js
@@ -2,9 +2,11 @@ import { shallowMount } from "@vue/test-utils";
 import CreatureCategories from "@/components/CreatureCategories.vue";
 
 describe("CourseForm.vue", () => {
-  it("If the category buttons are there", async () => { });
-  it("If the categorie buttons trigger select event (changing false -> true)", async () => { });
-  it("If the categorie buttons trigger deselect event (changing true -> false)", async () => { });
-  it("If buttons get colored if selected", async () => {});
-  it("If buttons get dis-colored if deselected", async () => {});
+  it("If the category check-boxes are there", async () => { });
+  it("If the categorie check-boxes trigger select event (changing false -> true)", async () => { });
+  it("If the categorie check-boxes trigger deselect event (changing true -> false)", async () => { });
+  it("If the select event (changing false -> true) emits", async () => { });
+  it("If the deselect event (changing true -> false) emits", async () => { });
+  it("If check-boxes get colored if selected", async () => {});
+  it("If check-boxes get dis-colored if deselected", async () => {});
 });
