074af114fe019c0abcd1a8415401b265cccb8083
VANIERCOLLEGE\2295439 <@edu.vaniercollege.qc.ca>
2023-08-10 17:20:54 -0400

    same v-model value

--- src/components/CreatureCategories.vue
+++ src/components/CreatureCategories.vue
@@ -6,29 +6,57 @@
     id="mammals"
     value="mammals"
     name="pickCategory"
+    v-model="chosen"
     >mammals</Input
   >
   <label for="birds"> birds</label>
-  <Input type="radio" id="birds" value="birds" name="pickCategory">birds</Input>
+  <Input
+    type="radio"
+    id="birds"
+    value="birds"
+    name="pickCategory"
+    v-model="chosen"
+    >birds</Input
+  >
   <label for="amphibians"> amphibians</label>
-  <Input type="radio" id="amphibians" value="amphibians" name="pickCategory"
+  <Input
+    type="radio"
+    id="amphibians"
+    value="amphibians"
+    name="pickCategory"
+    v-model="chosen"
     >amphibians</Input
   >
   <label for="reptiles"> reptiles</label>
-  <Input type="radio" id="reptiles" value="reptiles" name="pickCategory"
+  <Input
+    type="radio"
+    id="reptiles"
+    value="reptiles"
+    name="pickCategory"
+    v-model="chosen"
     >reptiles</Input
   >
   <label for="fishes"> fishes</label>
-  <Input type="radio" id="fishes" value="fishes" name="pickCategory"
+  <Input
+    type="radio"
+    id="fishes"
+    value="fishes"
+    name="pickCategory"
+    v-model="chosen"
     >fishes</Input
   >
+
+  <p>You picked: {{ chosen }}</p>
 </template>
 
 <script>
 export default {
   name: "CreatureCategories",
   data() {
-    return {};
+    return {
+      chosen: "mammals",
+    };
   },
+  methods: {},
 };
 </script>
