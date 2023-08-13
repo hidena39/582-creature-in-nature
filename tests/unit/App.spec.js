8cbe2bafb85513131dbd2e0a5aadae79067aba46
DIDI-LAPTOP\hiden <hidenadeluca@gmail.com>
2023-08-12 20:09:17 -0400

    test finished

--- tests/unit/App.spec.js
+++
@@ -1,6 +0,0 @@
-// import { shallowMount } from "@vue/test-utils";
-// import App from "@/App.vue";
-
-// describe("App.vue", () => {
-//   it("If it receive the data from Input.spec.js", () => {});
-// });
